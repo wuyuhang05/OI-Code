722be104e4c7de0a75df5491938205dd4a447f3c
RainAir <wuyuhang422@gmail.com>
2019-01-28 13:38:00 +0800

    Add solution for problem `D - Distance in Tree` Link: https://codeforces.com/contest/161/problem/D Tags: dfs and similar, dp, trees, *1800 Ref: https://codeforces.com/contest/161/submission/49079660

--- codeforces/161/D.cpp
+++ codeforces/161/D.cpp
@@ -1,69 +1,94 @@
-/*
- * Author: RainAir
- * Time: 2019-08-15 20:53:48
- */
 #include <algorithm>
 #include <iostream>
 #include <cstring>
 #include <climits>
-#include <cstdlib>
 #include <cstdio>
-#include <bitset>
 #include <vector>
-#include <cmath>
+#include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include <queue>
 #include <stack>
 #include <map>
 #include <set>
 
 #define fi first
+#define lc (x<<1)
 #define se second
 #define U unsigned
-#define P std::pair
+#define rc (x<<1|1)
 #define Re register
 #define LL long long
-#define pb push_back
 #define MP std::make_pair
-#define all(x) x.begin(),x.end()
 #define CLR(i,a) memset(i,a,sizeof(i))
 #define FOR(i,a,b) for(Re int i = a;i <= b;++i)
 #define ROF(i,a,b) for(Re int i = a;i >= b;--i)
+#define SFOR(i,a,b,c) for(Re int i = a;i <= b;i+=c)
+#define SROF(i,a,b,c) for(Re int i = a;i >= b;i-=c)
 #define DEBUG(x) std::cerr << #x << '=' << x << std::endl
-#define int LL
-const int MAXN = 1e5+5;
+
+const int MAXN = 50000+5;
 const int MAXK = 500+5;
 
 struct Edge{
-    int to,nxt;
+    int to,next;
 }e[MAXN<<1];
-int head[MAXN],n,k,cnt;
+int head[MAXN],size[MAXN],max[MAXN],dis[MAXN],cnt;
+int root,N,sum,K;
+bool vis[MAXN];
+int p[MAXN],st[MAXN];
 
 inline void add(int u,int v){
     e[++cnt] = (Edge){v,head[u]};head[u] = cnt;
-    e[++cnt] = (Edge){u,head[v]};head[v] = cnt;
 }
 
-int sz[MAXN],f[MAXN][MAXK],ans;
+inline void getroot(int v,int fa){
+    size[v] = 1;max[v] = 0;
+    for(int to,i = head[v];i;i = e[i].next){
+        if((to = e[i].to) == fa || vis[to]) continue;
+        getroot(to,v);size[v] += size[to];
+        max[v] = std::max(max[v],size[to]);
+    }
+    max[v] = std::max(max[v],sum-size[v]);
+    root = max[root] > max[v] ? v : root;
+}
 
-inline void dfs(int v,int fa=0){
-    sz[v] = 1;f[v][0] = 1;
-    for(int i = head[v];i;i = e[i].nxt){
-        if(e[i].to == fa) continue;
-        dfs(e[i].to,v);
-        FOR(x,0,k-1) ans += f[v][x]*f[e[i].to][k-x-1];
-        FOR(x,1,k) f[v][x] += f[e[i].to][x-1];
-        sz[v] += sz[e[i].to];
+inline void getdis(int v,int fa){
+    st[++st[0]] = dis[v];p[dis[v]]++;
+    for(int to,i = head[v];i;i = e[i].next){
+        if((to = e[i].to) == fa || vis[to]) continue;
+        dis[to] = dis[v] + 1;getdis(to,v);
+    }
+}
+LL ans = 0;
+inline int calc(int v,int val){
+    st[0] = 0;dis[v] = val;
+    getdis(v,0);int res = 0;
+    FOR(i,1,st[0]){
+        if(K >= st[i]) res += (st[i]*2 != K) ? 1ll*p[st[i]]*p[K-st[i]] : 1ll*p[st[i]]*(p[st[i]]-1)/2;
+        p[st[i]] = 0;
+        if(K >= st[i]) p[K-st[i]] = 0;
+    }
+    return res;
+}
+
+inline void divide(int v){
+    ans += calc(v,0);vis[v] = 1;
+    for(int to,i = head[v];i;i = e[i].next){
+        if(vis[(to = e[i].to)]) continue;
+        ans -= calc(to,1);max[root=0]=INT_MAX;sum = size[to];
+        getroot(to,0);divide(root);
     }
 }
 
-signed  main(){
-    scanf("%lld%lld",&n,&k);
-    FOR(i,1,n-1){
-        int u,v;scanf("%lld%lld",&u,&v);
-        add(u,v);
+int main(){
+    scanf("%d%d",&N,&K);max[root=0] = sum = N;
+    FOR(i,1,N-1){
+        int u,v;scanf("%d%d",&u,&v);
+        add(u,v);add(v,u);
     }
-    dfs(1);
-    printf("%lld\n",ans);
+    getroot(1,0);
+    divide(root);
+    printf("%I64d\n",ans);
     return 0;
 }
