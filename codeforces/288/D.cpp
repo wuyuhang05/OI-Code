eee50d7f83071eba78e43a88da4dc055eb270404
RainAir <wuyuhang422@gmail.com>
2019-07-09 14:22:00 +0800

    Add solution for problem `D - Polo the Penguin and Trees ` Link: https://codeforces.com/contest/288/problem/D Tags: combinatorics, dfs and similar, trees, *2400 Ref: https://codeforces.com/contest/288/submission/56737996

--- codeforces/288/D.cpp
+++ codeforces/288/D.cpp
@@ -1,67 +1,58 @@
-#include <algorithm>
-#include <iostream>
-#include <cstring>
-#include <climits>
-#include <cstdlib>
-#include <cstdio>
-#include <bitset>
-#include <vector>
-#include <cmath>
-#include <ctime>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
-
-#define fi first
-#define se second
-#define U unsigned
-#define P std::pair
-#define Re register
-#define LL long long
-#define pb push_back
-#define MP std::make_pair
-#define all(x) x.begin(),x.end()
-#define CLR(i,a) memset(i,a,sizeof(i))
-#define FOR(i,a,b) for(Re int i = a;i <= b;++i)
-#define ROF(i,a,b) for(Re int i = a;i >= b;--i)
-#define DEBUG(x) std::cerr << #x << '=' << x << std::endl
-#define int LL
-const int MAXN = 80000+5;
-std::vector<int> G[MAXN];
-int n;
-int sz[MAXN],fa[MAXN];
-
-inline void dfs1(int v,int faa=0){
-    sz[v] = 1;fa[v] = faa;
-    for(auto x:G[v]){
-        if(x == faa) continue;
-        dfs1(x,v);sz[v] += sz[x];
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<map>
+#include<set>
+#include<ctime>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int>P;
+const int INF=0x3f3f3f3f,maxn=80005;
+int n,size[maxn],fa[maxn];
+vector<int>g[maxn]; 
+void dfs(int u,int f)
+{
+    size[u]=1;
+    fa[u]=f;
+    for(int i=0;i<g[u].size();i++)
+    {
+        int v=g[u][i];
+        if(v==f)continue;
+        dfs(v,u);
+        size[u]+=size[v];
     }
 }
-LL ans = 0ll;
-
-inline LL C(LL x){
-    return 1ll*x*(x-1)/2;
+ll C(int n)
+{
+    return (ll)n*(n-1)/2;
 }
+int main()
+{
+    scanf("%d",&n);
+    for(int i=1;i<n;i++)
+    {
+        int u,v;
+        scanf("%d%d",&u,&v);
+        g[u].push_back(v),g[v].push_back(u);
+    }
+    dfs(1,0);
+    ll ans=C(n)*C(n);
 
-signed main(){
-    scanf("%lld",&n);
-    FOR(i,1,n-1){
-        int u,v;scanf("%lld%lld",&u,&v);
-        G[u].pb(v);G[v].pb(u);
-    }dfs1(1);ans = C(n)*C(n);
-    FOR(i,1,n){
-        LL num2 = C(sz[i]),num1 = C(n);
-        for(auto x:G[i]){
-            if(x == fa[i]){
-                num1 -= C(n-sz[i]);continue;
-            }
-            num2 -= C(sz[x]);num1 -= C(sz[x]);
+    for(int u=1;u<=n;u++)
+    {
+        ll num1=C(n),num2=C(size[u]);
+        for(int i=0;i<g[u].size();i++)
+        {
+            int v=g[u][i];
+            if(v!=fa[u])num1-=C(size[v]),num2-=C(size[v]);
+            else num1-=C(n-size[u]);
         }
-        ans -= (2ll*num1*num2-num2*num2);
-       // ans -= 2ll*num1*num2-num2*num2;// (num1- num2)*num2 + num2*num2;
-    }
-    std::cout << ans << std::endl;
+        ans-=(2ll*num1*num2-num2*num2);
+    } 
+    printf("%I64d\n",ans);
     return 0;
 }
