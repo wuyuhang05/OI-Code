d9596cdaf8e9a67904965e3d34079ed0755dafe5
RainAir <wuyuhang422@gmail.com>
2020-09-11 15:40:00 +0800

    Add solution for problem `E - Coins Exhibition` Link: https://codeforces.com/contest/930/problem/E Tags: data structures, dp, math, *2900 Ref: https://codeforces.com/contest/930/submission/92478134

--- codeforces/930/E.cpp
+++ codeforces/930/E.cpp
@@ -16,16 +16,13 @@
 
 const int MAXN = 5e5 + 5;
 const int ha = 1e9 + 7;
-int k,n,m;
-P a[2][MAXN];
+
 std::vector<int> S;
+int k,m0,m1,n;
+P a[2][MAXN];
 int lim[2][MAXN];
 int f[2][MAXN];
-// 当前这一位是0/1 上一个不一样的在哪里 
-
-inline void add(int &x,int y){
-	x += y;if(x >= ha) x -= ha;
-}
+// int f[2][105][105];
 
 inline int qpow(int a,int n=ha-2){
 	int res = 1;
@@ -37,46 +34,85 @@ inline int qpow(int a,int n=ha-2){
 	return res;
 }
 
+inline void add(int &x,int y){
+	x += y;if(x >= ha) x -= ha;
+}
+
 int main(){
-	scanf("%d%d%d",&k,&n,&m);
+	scanf("%d%d%d",&k,&m0,&m1);
+	FOR(i,1,m0){
+		scanf("%d%d",&a[0][i].fi,&a[0][i].se);
+		S.pb(a[0][i].fi-1);S.pb(a[0][i].se);
+	}
+	FOR(i,1,m1){
+		scanf("%d%d",&a[1][i].fi,&a[1][i].se);
+		S.pb(a[1][i].fi-1);S.pb(a[1][i].se);
+	}
 	S.pb(0);S.pb(k);
-	FOR(i,1,n){
-		scanf("%d%d",&a[0][i].fi,&a[0][i].se);--a[0][i].fi;
-		S.pb(a[0][i].fi);S.pb(a[0][i].se);
-	}
-	FOR(i,1,m){
-		scanf("%d%d",&a[1][i].fi,&a[1][i].se);--a[1][i].fi;
-		S.pb(a[1][i].fi);S.pb(a[1][i].se);
-	}
-	CLR(lim,-1);
 	std::sort(all(S));S.erase(std::unique(all(S)),S.end());
-	FOR(i,1,n){
-		a[0][i].fi = std::lower_bound(all(S),a[0][i].fi)-S.begin();
+	CLR(lim,-1);
+	FOR(i,1,m0){
+		a[0][i].fi = std::lower_bound(all(S),a[0][i].fi-1)-S.begin();
 		a[0][i].se = std::lower_bound(all(S),a[0][i].se)-S.begin();
 		lim[0][a[0][i].se] = std::max(lim[0][a[0][i].se],a[0][i].fi);
 	}
-	FOR(i,1,m){
-		a[1][i].fi = std::lower_bound(all(S),a[1][i].fi)-S.begin();
+	FOR(i,1,m1){
+		a[1][i].fi = std::lower_bound(all(S),a[1][i].fi-1)-S.begin();
 		a[1][i].se = std::lower_bound(all(S),a[1][i].se)-S.begin();
 		lim[1][a[1][i].se] = std::max(lim[1][a[1][i].se],a[1][i].fi);
 	}
-	int n = S.size();
+	n = S.size();
 	FOR(i,0,1) FOR(j,1,n) lim[i][j] = std::max(lim[i][j],lim[i][j-1]);
-	f[0][0] = 1;int sm0 = 1,tp0 = 0,sm1 = 0,tp1 = 0;
+	f[0][0] = 1;int sm0=1,tp0=0,sm1=0,tp1=0;
 	FOR(i,0,n-2){
-		while(tp0 <= lim[1][i]) add(sm0,ha-f[0][tp0]),++tp0;
-		while(tp1 <= lim[0][i]) add(sm1,ha-f[1][tp1]),++tp1;
-		add(f[1][i],sm0);add(f[0][i],sm1);
-		int gx = qpow(2,S[i+1]-S[i]-1);add(gx,ha-1);
+		while(tp0 <= lim[1][i]) add(sm0,ha-f[0][tp0]),tp0++;
+		while(tp1 <= lim[0][i]) add(sm1,ha-f[1][tp1]),tp1++;
+		int gx = (qpow(2,S[i+1]-S[i]-1)+ha-1)%ha;
 		gx = 1ll*gx*(sm0+sm1)%ha;
 		add(f[0][i+1],gx);add(f[1][i+1],gx);
+		add(f[0][i],sm1);add(f[1][i],sm0);
 		int t0 = sm0,t1 = sm1;
-		add(sm0,t1);add(sm1,t0);
 		add(sm0,gx);add(sm1,gx);
+		add(sm0,t1);add(sm1,t0);
 	}
+	// f[0][0][0] = 1;
+	// FOR(i,0,n-2){
+		// FOR(j,0,i){
+			// if(j > lim[1][i]){
+				// add(f[0][i+1][j],f[0][i][j]);
+				// add(f[1][i+1][i],f[0][i][j]);
+				// int gx = (qpow(2,S[i+1]-S[i]-1)+ha-1)%ha;
+				// gx = 1ll*gx*f[0][i][j]%ha;
+				// add(f[0][i+1][i+1],gx);
+				// add(f[1][i+1][i+1],gx);
+			// }
+			// else f[0][i][j] = 0;
+			// if(j > lim[0][i]){
+				// add(f[1][i+1][j],f[1][i][j]);
+				// add(f[0][i+1][i],f[1][i][j]);
+				// int gx = (qpow(2,S[i+1]-S[i]-1)+ha-1)%ha;
+				// gx = 1ll*gx*f[1][i][j]%ha;
+				// add(f[1][i+1][i+1],gx);
+				// add(f[0][i+1][i+1],gx);
+			// }
+			// else f[1][i][j] = 0;
+		// }
+	// }
+	// 1 2 3 5
+	// DEBUG(f[0][4][4]);DEBUG(f[0][4][3]);
 	int ans = 0;
 	FOR(i,0,n) if(i > lim[1][n]) add(ans,f[0][i]);
 	FOR(i,0,n) if(i > lim[0][n]) add(ans,f[1][i]);
 	printf("%d\n",ans);
+	// DEBUG(ans);
 	return 0;
 }
+
+/*
+f[0/1][i][j] 表示考虑到第i个点,上一个和这个点颜色不同的地方是j
+定义 i 所在的块是 (x[i-1],x[i]]
+pre 表示前面最远的哪一个块必须有0/1
+f[0][i][j] -> f[0][i+1][j](pre[1][i+1] <= j)
+f[0][i][j] -> f[1][i+1][i](pre[0][i+1] <= j)
+(2^{x[i+1]-x[i]-1}-1)f[0][i][j] -> f[0][i+1][i+1]\f[1][i+1][i+1](pre[1][i+1] <= j\pre[0][i+1] <= j)
+*/
