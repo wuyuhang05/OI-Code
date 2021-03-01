162a52a5f81a8af2d14c7be14aab7a2758d51dcb
RainAir <wuyuhang422@gmail.com>
2021-03-01 11:46:00 +0800

    Add solution for problem `E - Cartesian Tree ` Link: https://codeforces.com/contest/1290/problem/E Tags: data structures, *3300 Ref: https://codeforces.com/contest/1290/submission/108767082

--- codeforces/1290/E.cpp
+++ codeforces/1290/E.cpp
@@ -17,155 +17,121 @@
 #define DEBUG(x) std::cerr << #x << '=' << x << std::endl
 
 const int MAXN = 2e5 + 5;
-int ch[MAXN][2],f[MAXN],sz[MAXN],tsz[MAXN],vsz[MAXN],a[MAXN];
-LL vans[MAXN],tans[MAXN];
-#define lc (ch[x][0])
-#define rc (ch[x][1])
-// t: tree 
-// v: virtual
 
-inline bool nroot(int x){
-    return ch[f[x]][0]==x||ch[f[x]][1]==x;
-}
+struct DS{
+    #define lc ((x)<<1)
+    #define rc ((x)<<1|1)
+    LL sm[MAXN<<2];
+    int cnt[MAXN<<2],tag[MAXN<<2];
+    int mx[MAXN<<2],cmx[MAXN<<2],mxc[MAXN<<2];
 
-inline void pushup(int x){
-    sz[x] = sz[lc]+sz[rc]+1;
-    tsz[x] = tsz[lc]+tsz[rc]+vsz[x]+1;
-    tans[x] = vans[x]+tans[lc]+tans[rc]+1ll*(sz[lc]+1)*(tsz[rc]+vsz[x]+1);
-//    tans[x] = vans[x]+tans[lc]+tans[rc]+tsz[rc]+vsz[x]+1;
-}
-
-inline void rotate(int x){
-    int y = f[x],z = f[y],k = ch[y][1]==x,w = ch[x][!k];
-    if(nroot(y)) ch[z][ch[z][1]==y] = x;f[x] = z;
-    ch[x][!k] = y;f[y] = x;
-    ch[y][k] = w;if(w) f[w] = y;
-    pushup(y);pushup(x);
-}
-
-inline void splay(int x,int v=0){
-    while(nroot(x)){
-        int y = f[x],z = f[y];
-        if(nroot(y)) rotate((ch[z][1]==y)^(ch[y][1]==x)?x:y);
-        rotate(x);
+    inline void pushup(int x){
+        sm[x] = sm[lc]+sm[rc];cnt[x] = cnt[lc]+cnt[rc];
+        if(mx[lc] < mx[rc]){
+            mx[x] = mx[rc];mxc[x] = mxc[rc];
+            cmx[x] = std::max(mx[lc],cmx[rc]);
+        }
+        else if(mx[lc] == mx[rc]){
+            mx[x] = mx[lc];mxc[x] = mxc[lc]+mxc[rc];
+            cmx[x] = std::max(cmx[lc],cmx[rc]);
+        }
+        else{
+            mx[x] = mx[lc];mxc[x] = mxc[lc];
+            cmx[x] = std::max(cmx[lc],mx[rc]);
+        }
     }
-}
 
-inline void access(int x){
-    for(int y = 0;x;x = f[y=x]){
-        splay(x);
-        vsz[x] += tsz[rc];
-        vans[x] += tans[rc];
-        rc = y;
-        vsz[x] -= tsz[y];
-        vans[x] -= tans[y];
+    inline void build(int x,int l,int r){
+        tag[x] = 0;
+        if(l == r){
+            sm[x] = cnt[x] = 0;
+            mx[x] = cmx[x] = -1e9;mxc[x] = 0;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        build(lc,l,mid);build(rc,mid+1,r);
         pushup(x);
     }
-}
 
-inline void cut(int v){// cut(v,fa[v])
-    access(v);splay(v);
-    f[ch[v][0]] = 0;
-    ch[v][0] = 0;
-    pushup(v);
-}
-
-inline void link(int u,int v){// set u as v's father
-    access(u);splay(u);
-    f[v] = u;ch[u][1] = v;
-    pushup(u);
-}
-
-inline int findpre(int x,int v){// 第一个 > v 的
-    int ans = 0;
-    while(x){
-        if(a[x] > v) ans = x,x = rc;
-        else x = lc;
+    inline void update(int x,int l,int r,int p,int d){
+        if(l == r){
+            sm[x] = mx[x] = d;cnt[x] = 1;mxc[x] = 1;
+            return;
+        }
+        int mid = (l + r) >> 1;pushdown(x);
+        if(p <= mid) update(lc,l,mid,p,d);
+        else update(rc,mid+1,r,p,d);
+        pushup(x);
     }
-    return ans;
-}
 
-inline int findsuf(int x,int v){
-    int ans = 0;
-    while(x){
-        if(a[x] < v) ans = x,x = lc;
-        else x = rc;
+    inline void cover1(int x,int d){// 区间+=d
+        if(!cnt[x]) return;
+        sm[x] += 1ll*cnt[x]*d;mx[x] += d;if(cmx[x] != -1e9) cmx[x] += d;
+        tag[x] += d;
     }
-    return ans;
-}
 
-int n,ps[MAXN];
-std::set<int> S;
-int ls[MAXN],rs[MAXN];// for tree
+    inline void cover2(int x,int d){// 区间max(d,*)
+        if(mx[x] <= d || !cnt[x]) return;
+        assert(cmx[x] < d);
+        sm[x] += 1ll*(d-mx[x])*mxc[x];
+        mx[x] = d;
+    }
 
-inline void dfs(int x){
-    if(ls[x]) dfs(ls[x]),vans[x] += tans[ls[x]],vsz[x] += tsz[ls[x]];
-    if(rs[x]) dfs(rs[x]),vans[x] += tans[rs[x]],vsz[x] += tsz[rs[x]];
-    pushup(x);
-}
+    inline void pushdown(int x){
+        if(tag[x]){
+            cover1(lc,tag[x]);
+            cover1(rc,tag[x]);
+            tag[x] = 0;
+        }
+        cover2(lc,mx[x]);
+        cover2(rc,mx[x]);
+    }
 
-inline void print(int x,std::vector<int> &v){
-    if(lc) print(lc,v);
-    v.pb(x);
-    if(rc) print(rc,v);
-}
+    inline void modify1(int x,int l,int r,int L,int R,int d){
+        if(L > R || !cnt[x]) return;
+        if(l == L && r == R) return cover1(x,d);
+        int mid = (l + r) >> 1;pushdown(x);
+        if(R <= mid) modify1(lc,l,mid,L,R,d);
+        else if(L > mid) modify1(rc,mid+1,r,L,R,d);
+        else modify1(lc,l,mid,L,mid,d),modify1(rc,mid+1,r,mid+1,R,d);
+        pushup(x);
+    }
 
-LL res[MAXN];
+    inline void modify2(int x,int l,int r,int L,int R,int d){
+        if(L > R || !cnt[x] || mx[x] <= d) return;
+        if(l == L && r == R && cmx[x] < d) return cover2(x,d);
+        int mid = (l + r) >> 1;pushdown(x);
+        if(R <= mid) modify2(lc,l,mid,L,R,d);
+        else if(L > mid) modify2(rc,mid+1,r,L,R,d);
+        else modify2(lc,l,mid,L,mid,d),modify2(rc,mid+1,r,mid+1,R,d);
+        pushup(x);
+    }
+
+    inline int query(int x,int l,int r,int L,int R){
+        if(l == L && r == R) return cnt[x];
+        int mid = (l + r) >> 1;pushdown(x);
+        if(R <= mid) return query(lc,l,mid,L,R);
+        if(L > mid) return query(rc,mid+1,r,L,R);
+        return query(lc,l,mid,L,mid)+query(rc,mid+1,r,mid+1,R);
+    }
+}dl,dr;
+
+int n,a[MAXN],ps[MAXN];
 
 int main(){
     scanf("%d",&n);
-    FOR(i,1,n) scanf("%d",a+i),ps[a[i]] = i,S.insert(i),sz[i] = 1;
-    static int st[MAXN],tp = 0;
+    FOR(i,1,n) scanf("%d",a+i),ps[a[i]] = i;
+    dl.build(1,1,n);dr.build(1,1,n);
     FOR(i,1,n){
-        int k = tp;
-        while(tp && a[st[tp]] < a[i]) --tp;
-        if(tp) rs[st[tp]] = i,f[i] = st[tp];
-        if(tp != k) ls[i] = st[tp+1],f[st[tp+1]] = i;
-        st[++tp] = i;
+        int p = ps[i],rk = dr.query(1,1,n,1,p)+1;
+        dr.update(1,1,n,p,i);
+        dl.update(1,1,n,p,-1);
+        dr.modify1(1,1,n,p+1,n,1);
+        dl.modify1(1,1,n,p+1,n,-1);
+        dr.modify2(1,1,n,1,p-1,rk-1);
+        dl.modify2(1,1,n,p+1,n,-(rk+1));
+        printf("%lld\n",dr.sm[1]+dl.sm[1]+i);
+//        if(i == 2) return 0;
     }
-    assert(st[1] == ps[n]);
-    dfs(ps[n]);
-    ROF(i,n,2){
-        int p = ps[i];
-        access(p);splay(p);res[i] = tans[p];
-        auto it = S.find(p);
-        if(next(it) == S.end() || it == S.begin()){
-            int y = ps[i-1];
-            access(y);splay(y);
-            cut(y);
-        }
-        else{
-            int y = *prev(it),z = *next(it);
-            access(y);splay(y);int sy = sz[y];
-            access(z);splay(z);int sz = ::sz[z];
-            // sy -> sx
-            if(sy < sz) std::swap(y,z),std::swap(sy,sz);
-            access(z);splay(z);
-            std::vector<int> vec;print(z,vec);
-            for(auto x:vec) if(x != p) cut(x);
-            std::reverse(all(vec));
-            int now = y;
-            for(auto x:vec){
-                if(x == p) continue;
-                access(now);splay(now);
-                int pre = findpre(now,a[x]),suf = findsuf(now,a[x]);
-                splay(pre);
-                if(suf) splay(suf);
-                // 这样才能定位(pre,suf)
-                if(suf) cut(suf);
-                if(pre) link(pre,x);
-                if(suf) link(x,suf);
-                now = x;// finger search
-            }
-            now = y;
-            access(now);splay(now);
-            int t = findsuf(now,i);
-            splay(t);
-            cut(t);
-        }
-        S.erase(p);
-    }
-    res[1] = 1;
-    FOR(i,1,n) printf("%lld\n",res[i]);
     return 0;
 }
